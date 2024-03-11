dc38cd92b4d246103b83ca9e53c543b41a9f39bb
Raunak Rajput <78155138+raunakr11@users.noreply.github.com>
2024-03-11 23:19:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const nav = document.querySelector('nav');
+  const skillsSection = document.getElementById('skills-section');
+ 
+  const observer = new IntersectionObserver(
+     (entries) => {
+       entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+           nav.classList.add('inverted');
+         } else {
+           nav.classList.remove('inverted');
+         }
+       });
+     },
+     {
+       rootMargin: '0px 0px -550px 0px', // Adjust the threshold for triggering the color inversion
+     }
+  );
+ 
+  observer.observe(skillsSection);
+ });
+ 
